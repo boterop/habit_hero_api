bc3db7f63de9e5c27033e3dad7106cd67b51d7f2
boterop <boterop22@gmail.com>
2024-05-28 17:31:43 -0500

    Fix mkdir func

--- lib/habit_hero_api/image.ex
+++ lib/habit_hero_api/image.ex
@@ -9,8 +9,19 @@ defmodule Image do
   def save_base64_image(image_name, base64) do
     {:ok, binary} = Base.decode64(base64)
     path = get_static_path(image_name)
-    File.mkdir_p(@image_path)
+    mkdir(@static_path)
     File.write(path, binary)
+  end
+
+  @spec mkdir(binary()) :: :ok | {:error, atom()}
+  def mkdir(path) do
+    path
+    |> String.split("/")
+    |> Enum.reduce("", fn dir, acc ->
+      dir_path = acc <> dir <> "/"
+      File.mkdir_p(dir_path)
+      dir_path
+    end)
   end
 
   @spec get_image(binary()) :: {:error, atom()} | {:ok, binary()}
